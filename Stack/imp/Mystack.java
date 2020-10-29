d1d0957c5d883ee519ef9be06890d1fadfd169b1
SanketGawande27 <53644173+SanketGawande27@users.noreply.github.com>
2020-10-29 14:46:42 +0530

    Added exceptions on business class

--- Stack/imp/Mystack.java
+++ Stack/imp/Mystack.java
@@ -4,7 +4,7 @@ import stack.customException.MyException;
 
 public class Mystack {
 
-	final int MAX = 10;
+	final int MAX = 5;
 	int top = -1;
 	int stk[] = new int[MAX];
 	int count = 0;
@@ -12,7 +12,7 @@ public class Mystack {
 
 	public int insert(int element) throws MyException {
 		if (top >= (MAX - 1)) {
-			throw new MyException("Stack is already Full");
+			throw new MyException("Stack is full no element is added");
 		} else {
 			stk[++top] = element;
 			count++;
@@ -23,7 +23,7 @@ public class Mystack {
 
 	public int delete() throws MyException {
 		if (top < 0) {
-			throw new MyException("Stack is already Empty");
+			throw new MyException("Stack is already Empty element not been deleted");
 		} else {
 			element = stk[top--];
 			count--;
